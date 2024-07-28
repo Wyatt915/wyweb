b656d9b0e773bfe993bb84cfee7adf32748ba0cc
Wyatt Sheffield <cws@wyatts.xyz>
2024-07-27 20:24:10 -0500

    GPLv3

--- extensions/extPriority.go
+++ extensions/extPriority.go
@@ -1,3 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                               //
+//                                                                                               //
+//         oooooo   oooooo     oooo           oooooo   oooooo     oooo         .o8               //
+//          `888.    `888.     .8'             `888.    `888.     .8'         "888               //
+//           `888.   .8888.   .8' oooo    ooo   `888.   .8888.   .8' .ooooo.   888oooo.          //
+//            `888  .8'`888. .8'   `88.  .8'     `888  .8'`888. .8' d88' `88b  d88' `88b         //
+//             `888.8'  `888.8'     `88..8'       `888.8'  `888.8'  888ooo888  888   888         //
+//              `888'    `888'       `888'         `888'    `888'   888    .o  888   888         //
+//               `8'      `8'         .8'           `8'      `8'    `Y8bod8P'  `Y8bod8P'         //
+//                                .o..P'                                                         //
+//                                `Y8P'                                                          //
+//                                                                                               //
+//                                                                                               //
+//                              Copyright (C) 2024  Wyatt Sheffield                              //
+//                                                                                               //
+//                 This program is free software: you can redistribute it and/or                 //
+//                 modify it under the terms of the GNU General Public License as                //
+//                 published by the Free Software Foundation, either version 3 of                //
+//                      the License, or (at your option) any later version.                      //
+//                                                                                               //
+//                This program is distributed in the hope that it will be useful,                //
+//                 but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+//                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 //
+//                          GNU General Public License for more details.                         //
+//                                                                                               //
+//                   You should have received a copy of the GNU General Public                   //
+//                         License along with this program.  If not, see                         //
+//                                <https://www.gnu.org/licenses/>.                               //
+//                                                                                               //
+//                                                                                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 package extensions
 
 /*
